d3fe437d93e18925b7dc743f8d97b4a75d63199e
Batu <ba921@nyu.edu>
2014-05-15 17:24:57 +0400

    commented out buildVGraph.cpp

--- buildVGraph.cpp
+++ buildVGraph.cpp
@@ -6,8 +6,11 @@
  */
 
 /*
- * C++ program to Implement AVL Tree
+ * C++ program to Implement AVL Tree that takes edges and connects runs
+ * visibility algorithm on the vector of nodes.
  */
+
+// Here are our dependencies.
 #include<iostream>
 #include<cmath>
 #include<sstream>
@@ -25,7 +28,10 @@ using namespace std;
 
 
 /*
- * Node Declaration
+ * Node Declaration:
+ * We store the x and y values in the node and in addition to that we store
+ * a vector of nodes that are connected to the node. These will be carrying
+ * our information.
  */
 
 class Node{
@@ -37,6 +43,10 @@ public:
 
 /*
  * AvlNode Declaration
+ * We create avl_nodes with the the abilty to store edges as a vector of nodes.
+ * An edge is represented as <Node1,Node2>. In addiion to that the node stores
+ * left and right, pointing to its left and right children nodes for the avl tree
+ *  to work
  */
 
 struct avl_node{
@@ -48,6 +58,14 @@ struct avl_node{
 
 /*
  * Class Declaration
+ *  height, diff, rotations, balance are necesary for the vanilla avl tree to work
+ *  Insert function is tailored to take a vector of Node as an edge. The
+ *  avl_tree puts the edges based on their distance to the base node. That comparrison
+ *  is done using compareDistanceBigger.
+ *  Display is a funciton that displays the current avl_Tree. Implemented for
+ *  testing purposes.
+ *  Is in fucntion checks whether an edge is in the Avl_tree.
+ *  Is visible creates the individual visibility graph for the nodes.
  */
 class avlTree{
     public:
@@ -61,9 +79,6 @@ class avlTree{
         avl_node* insert(avl_node *, Node*, vector<Node*>);
         avl_node* findMin(avl_node*);
         void display(avl_node *, int);
-        void inorder(avl_node *);
-        void preorder(avl_node *);
-        void postorder(avl_node *);
         void isVisible(avl_node*, vector<Node*>*, Node*, Node*);
         bool isIn(avl_node*, vector<Node*>);
         avl_node* delet(avl_node*,Node*,vector<Node*>);
@@ -74,6 +89,8 @@ class avlTree{
 };
 
 
+// Compares the relative angles of all the nodes to a base node whicih is
+// used in the first step of sorting.
 bool compareAngleBigger(Node* base, Node* node1, Node* node2){
 
 	double relx1, relx2, rely1, rely2;
@@ -114,111 +131,114 @@ public:
 
 /*
  * Main Contains Menu
+ 	 !!TEST DATA!!
  */
 
 
-//	  int choice;
+	  int choice;
 
-//    Node base, node1, node2, node3, node4, node5;
-//    vector <Node*> node1vec, node2vec, node3vec, node4vec;
-//    vector<Node*> visibilityList;
+    Node base, node1, node2, node3, node4, node5;
+    vector <Node*> node1vec, node2vec, node3vec, node4vec;
+    vector<Node*> visibilityList;
 
-//    base.x = 0;
-//    base.y = 0;
-//
-//    node1.x = 11;
-//    node1.y = 35;
-//    node1.adjNode.push_back(&node4);
-//    node1.adjNode.push_back(&node2);
-//
-//
-//    node2.x = 22;
-//    node2.y = 43;
-//
-//    node2.adjNode.push_back(&node1);
-//    node2.adjNode.push_back(&node3);
-//
-//    node3.x = 34;
-//    node3.y = 42;
-//    node3.adjNode.push_back(&node2);
-//    node3.adjNode.push_back(&node4);
-//
-//
-//    node4.x = 45;
-//    node4.y = 36;
-//    node4.adjNode.push_back(&node3);
-//    node4.adjNode.push_back(&node3);
+    base.x = 0;
+    base.y = 0;
+
+    node1.x = 11;
+    node1.y = 35;
+    node1.adjNode.push_back(&node4);
+    node1.adjNode.push_back(&node2);
+
+
+    node2.x = 22;
+    node2.y = 43;
+
+    node2.adjNode.push_back(&node1);
+    node2.adjNode.push_back(&node3);
+
+    node3.x = 34;
+    node3.y = 42;
+    node3.adjNode.push_back(&node2);
+    node3.adjNode.push_back(&node4);
+
+
+    node4.x = 45;
+    node4.y = 36;
+    node4.adjNode.push_back(&node3);
+    node4.adjNode.push_back(&node3);
 
 	avlTree avl;
+	vector<Node*> nodeList;
 
 	//Creating the main loop that will iterate through all nodes
 	for (int m = 0;m < nodeList.size(); m++){
 	}
 
-//    while (1)
-//    {
-//        cout<<"\n---------------------"<<endl;
-//        cout<<"AVL Tree Implementation - Batufied"<<endl;
-//        cout<<"\n---------------------"<<endl;
-//        cout<<"1.Insert Element into the tree -Batu"<<endl;
-//        cout<<"2.Display Balanced AVL Tree"<<endl;
-//        cout<<"6.Find the minimum - Batu"<<endl;
-//        cout<<"7.Delete and pray v2 - Batu"<<endl;
-//        cout<<"8.is it in -Batu"<<endl;
-//        cout<<"9.visibility graph come on" <<endl;
-//        cout<<"10.Exit"<<endl;
-//        cout<<"Enter your Choice: ";
-//        cin>>choice;
-//        switch(choice)
-//        {
-//        case 1:
-//            cout<<"Enter value to be inserted: ";
-//            break;
-//        case 2:
-//            if (root == NULL)
-//            {
-//                cout<<"Tree is Empty"<<endl;
-//                continue;
-//            }
-//            cout<<"Balanced AVL Tree:"<<endl;
-//            avl.display(root, 1);
-//            break;
-//        case 6:
-//            cout<<"Minimum:"<<endl;
-//            int x;
-//            x = avl.findMin(root)->node->x;
-//            cout << x;
-//            cout<<endl;
-//            break;
-//        case 7:
-//            cout<<"Enter value to be deleted: ";
-//            root = avl.delet(root, &base, &node2);
-//            root = avl.delet(root, &base, &node3);
-//            root = avl.delet(root, &base, &node4);
-//            root = avl.delet(root, &base, &node1);
-//            break;
-//        case 8:
-//            avl.isIn(root, &node1);
-//            avl.isIn(root, &node2);
-//            avl.isIn(root, &node3);
-//            avl.isIn(root, &node4);
-//            cout << "No crushes" << endl;
-//            break;
-//        case 9:
-//        	avl.isVisible(root, &visibilityList, &base, &node1);
-//        	cout << visibilityList[0]->x;
-//        	break;
-//        case 10:
-//            exit(1);
-//            break;
-//        default:
-//            cout<<"Wrong Choice"<<endl;
-//        }
-//    }
+    while (1)
+    {
+        cout<<"\n---------------------"<<endl;
+        cout<<"AVL Tree Implementation - Batufied"<<endl;
+        cout<<"\n---------------------"<<endl;
+        cout<<"1.Insert Element into the tree -Batu"<<endl;
+        cout<<"2.Display Balanced AVL Tree"<<endl;
+        cout<<"6.Find the minimum - Batu"<<endl;
+        cout<<"7.Delete and pray v2 - Batu"<<endl;
+        cout<<"8.is it in -Batu"<<endl;
+        cout<<"9.visibility graph come on" <<endl;
+        cout<<"10.Exit"<<endl;
+        cout<<"Enter your Choice: ";
+        cin>>choice;
+        switch(choice)
+        {
+        case 1:
+            cout<<"Enter value to be inserted: ";
+            break;
+        case 2:
+            if (root == NULL)
+            {
+                cout<<"Tree is Empty"<<endl;
+                continue;
+            }
+            cout<<"Balanced AVL Tree:"<<endl;
+            avl.display(root, 1);
+            break;
+        case 6:
+            cout<<"Minimum:"<<endl;
+            int x;
+            x = avl.findMin(root)->node->x;
+            cout << x;
+            cout<<endl;
+            break;
+        case 7:
+            cout<<"Enter value to be deleted: ";
+
+            break;
+        case 8:
+            cout << "No crushes" << endl;
+            break;
+        case 9:
+        	avl.isVisible(root, &visibilityList, &base, &node4);
+        	avl.isVisible(root, &visibilityList, &base, &node3);
+        	avl.isVisible(root, &visibilityList, &base, &node2);
+        	avl.isVisible(root, &visibilityList, &base, &node1);
+        	cout << "printing results :" << endl;
+        	cout << visibilityList.size() << endl;
+        	cout << visibilityList[0]->x << endl;
+        	cout << visibilityList[1]->x << endl;
+        	break;
+        case 10:
+            exit(1);
+            break;
+        default:
+            cout<<"Wrong Choice"<<endl;
+        }
+    }
 	return 0;
 }
 
-
+// The function that compares the relative distances between 3 nodes
+// An example would be: distance AB and distance AC is compared. A being the
+// base in this case.
 bool compareDistanceBiggerNode( Node* base,  Node* node1, Node* node2)
 {
 
@@ -234,6 +254,10 @@ bool compareDistanceBiggerNode( Node* base,  Node* node1, Node* node2)
 	}
 }
 
+
+// The function checks the mid point of the edges and finds the relative distance
+// of those points to the base node. This is used as a comparrison function
+// for the avl_tree.
 bool compareDistanceEdge(Node* base,  vector<Node*> edge1, vector<Node*> edge2 )
 {
 
@@ -256,10 +280,22 @@ bool compareDistanceEdge(Node* base,  vector<Node*> edge1, vector<Node*> edge2 )
 }
 
 
+/*
+* isVisible function is the main logic engine for this set of functions.
+* It takes the avl_root to be able to work on the avl tree. An empty vector
+* called Visibility vector of nodes to output the results into it.
+* A base node that acts as the anchor point and the point where the function is
+* based on
+* and a target node that is compared to the base node to see whether it is visible
+* or not.
+*/
+void avlTree::isVisible(avl_node* root, vector<Node*>* visibilityVector, Node* base, Node* target)
+{
 
-void avlTree::isVisible(avl_node* root, vector<Node*>* visibilityVector, Node* base, Node* target){
-
-
+	// each node carries a vector of 2 nodes that is connected to them. The following
+	// five line uses that informatin to create two edges. Remember our edge
+	//representation is <Node1,Node2> and in this case node 1 is always the target
+	// node itself and node two is the node that is connected to it creating the edge.
 	vector<Node*> firstEdge, secondEdge;
 	firstEdge.push_back(target);
 	firstEdge.push_back(target->adjNode[0]);
@@ -267,35 +303,46 @@ void avlTree::isVisible(avl_node* root, vector<Node*>* visibilityVector, Node* b
 	secondEdge.push_back(target);
 	secondEdge.push_back(target->adjNode[1]);
 
+	//The algorithm first starts by checking whether the smallest element of the
+	// avl tree is NULL, which means the avl_tree is empty. In that case it
+	// inserts the edge one and two to the avl tree. It uses the insert function
+	// to sort them based on their distance and insert them to the avl tree acoordingly.
 	if (this->findMin(root) == NULL){
 		root = this->insert(root, base, firstEdge);
-		this->display(root,1);
 		root = this->insert(root, base, secondEdge);
-		this->display(root,1);
-		if (this->findMin(root)->node == target){
-	    	visibilityVector->push_back(target);
+	// after insertion the left most node is checked once again. If an edge is in
+	// the left most node this means that it is visible. If that is the case
+	// it is put into visiblity vector as an element of visibility list.
 	}
 	if ((this->findMin(root)->edge == firstEdge) or (this->findMin(root)->edge == secondEdge)){
 		visibilityVector->push_back(target);
+	// If they are not in the left most node they can still be in the avl tree
+	// on a different node. The next lines check the avl tree for each of the
+	// edges ad if they are in it it deletes the edge from the list.
+	// once again after this the left most is checked.
 	} else if(isIn(root, firstEdge)) {
 	   root = this->delet(root, base, firstEdge);
-	   this->display(root,1);
 	    if (this->findMin(root)->edge == firstEdge){
 	    	visibilityVector->push_back(target);
 	    }
 	} else if(isIn(root, secondEdge)){
 	    root = this->delet(root, base, secondEdge);
-	    this->display(root,1);
 	    if (this->findMin(root)->edge == secondEdge){
 	    	visibilityVector->push_back(target);
-	}else {
+	}else
+	// if neither of the edges are found in the avl tree they are inserted
+	// into the avl tree and the leftmost element is checked once again.
+		{
 		root = this->insert(root, base, firstEdge);
-		this->display(root,1);
 		root = this->insert(root, base, secondEdge);
-		this->display(root,1);
 		if (this->findMin(root)->node == target){
 	    	visibilityVector->push_back(target);
-}}}}}
+}}}}
+
+//////////////////////////////////////////////////////////////////////////////
+///////////////// AVL TREE IMPLEMENTATION ////////////////////////////////////
+// These functions are required for the avl tree to function ////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 /*
  * Height of AVL Tree
@@ -394,13 +441,11 @@ avl_node *avlTree::balance(avl_node *temp)
 
 /*
  * Insert Element into the tree
+ * using the compareDistanceEdge function as a comparrsion method when picking
+ * the right spot in the avl tree.
  */
 
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////// 						INSERT                        /////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
+
 avl_node *avlTree::insert(avl_node *root, Node* base, vector<Node*> edge)
 {
 
@@ -431,7 +476,6 @@ avl_node* avlTree::findMin(avl_node* root)
 {
 	if (root == NULL)
 	{
-		cout<<"The tree is empty\n"<<endl;
 		return NULL;
 	}
 	else
@@ -466,9 +510,7 @@ void avlTree::display(avl_node *ptr, int level)
 }
 
 bool avlTree::isIn(avl_node* root, vector<Node*> target){
-	cout << "Girdik isine";
 	if (root->edge == target){
-		cout << "Vallahi burada." << endl;
 		return true;
 
 	} else {
